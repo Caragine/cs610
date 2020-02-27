8a5fb120273abc6b1bb1862e8e46a559d83c2ff4
rmartins85 <50893308+rmartins85@users.noreply.github.com>
2020-02-27 12:51:40 -0500

    adding class Account and changing BankAccount

--- Project 02/BankAccount.java
+++ Project 02/BankAccount.java
@@ -3,43 +3,40 @@ import java.rmi.server.*;
 import java.util.ArrayList;
 
 public class BankAccount extends UnicastRemoteObject implements AccountInterface {
-	
-	ArrayList<BankAccount> accounts = new ArrayList<BankAccount>();
-   private String name;
-   private float balance;
-   public int accountNo;
-	  
+
+	private ArrayList<Account> accounts = new ArrayList<>();
+
 	public BankAccount() throws RemoteException {
-		super();		
+		super();
 	}
 		
 	public String createAccount(String name, float balance) throws RemoteException {
 		// In creating an account, it is the bank that selects an account number. In our project the first new account is 100, the next is 101, next is 102, ...
 		// Anyone can open a new account. It does not matter if the person has already accounts.
 		// In that case I created an ArrayList of object bankAccounts itself in order to control the account number of each object.
-		accounts.add(new BankAccount());
+		accounts.add(new Account());
 		for (int i=0; i<accounts.size(); i++) {
 			if (accounts.size() == 1) {
-				accounts.get(0).accountNo = 100;
-				accounts.get(0).name = name;
-				accounts.get(0).balance = balance;
+				accounts.get(0).setAccountNo(100);
+				accounts.get(0).setName(name);
+				accounts.get(0).setBalance(balance);
 			} else {
-				accounts.get(i).accountNo = 100+i;
-				accounts.get(i).name = name;
-				accounts.get(i).balance = balance;
+				accounts.get(i).setAccountNo(100+i);
+				accounts.get(i).setName(name);
+				accounts.get(i).setBalance(balance);
 			}
 		}
-		return "AccountNo: " + Integer.toString(accounts.get(-1).accountNo) +
-				"\nAccountName: " + accounts.get(-1).name + 
-				"\nBalance: " + accounts.get(-1).balance;
+		return "AccountNo: " + Integer.toString(accounts.get(-1).getAccountNo()) +
+				"\nAccountName: " + accounts.get(-1).getName() +
+				"\nBalance: " + accounts.get(-1).getBalance();
 	}
   
 	public String getBalance(String name, int accountNo) throws RemoteException {
 		// If the name with accountNo is in the bank return the person balance as a string otherwise either name has no account or his account no is wrong.  
 		String string = " ";
 		for (int i=0; i<accounts.size(); i++) {
-			if (accounts.get(i).name.equalsIgnoreCase(name) && accounts.get(i).accountNo == accountNo) {
-				string = "Account number " + accounts.get(i).accountNo + "'s balance: " + accounts.get(i).balance;
+			if (accounts.get(i).getName().equalsIgnoreCase(name) && accounts.get(i).getAccountNo() == accountNo) {
+				string = "Account number " + accounts.get(i).getAccountNo() + "'s balance: " + accounts.get(i).getBalance();
 			} else {
 				string = "Either name has no account or account number is wrong";
 			}
@@ -53,12 +50,12 @@ public class BankAccount extends UnicastRemoteObject implements AccountInterface
 		// There is enough funds. Withdraw and return the new balance.  
 		String string = " ";
 		for (int i=0; i<accounts.size(); i++) {
-			if (accounts.get(i).name.equalsIgnoreCase(name) && accounts.get(i).accountNo == accountNo) {
-				if (amt <= accounts.get(i).balance) {
-					accounts.get(i).balance -= amt;
-					string = "Account number " + accounts.get(i).accountNo + "'s new balance: " + accounts.get(i).balance;
+			if (accounts.get(i).getName().equalsIgnoreCase(name) && accounts.get(i).getAccountNo()== accountNo) {
+				if (amt <= accounts.get(i).getBalance()) {
+					accounts.get(i).setBalance(accounts.get(i).getBalance()-amt);
+					string = "Account number " + accounts.get(i).getAccountNo() + "'s new balance: " + accounts.get(i).getBalance();
 				} else {
-					string = "Insufficient funds! Balance: " + accounts.get(i).balance;
+					string = "Insufficient funds! Balance: " + accounts.get(i).getBalance();
 				}
 			}
 		}
@@ -70,9 +67,9 @@ public class BankAccount extends UnicastRemoteObject implements AccountInterface
 		// If account number with the name is in the bank, deposit and return the new balance.
 		String string = " ";
 		for (int i=0; i<accounts.size(); i++) {
-			if (accounts.get(i).name.equalsIgnoreCase(name) && accounts.get(i).accountNo == accountNo) {
-				accounts.get(i).balance += amt;
-				string = "Account number " + accounts.get(i).accountNo + "'s new balance: " + accounts.get(i).balance;
+			if (accounts.get(i).getName().equalsIgnoreCase(name) && accounts.get(i).getAccountNo()== accountNo) {
+				accounts.get(i).setBalance(accounts.get(i).getBalance()+amt);
+				string = "Account number " + accounts.get(i).getAccountNo()+ "'s new balance: " + accounts.get(i).getBalance();
 			} else {
 				string = "Wrong account number!";
 			}
@@ -86,20 +83,20 @@ public class BankAccount extends UnicastRemoteObject implements AccountInterface
 		// There is enough funds. Withdraw and return the new balance.
 		String string = " ";
 		for (int i=0; i<accounts.size(); i++) {
-			if (accounts.get(i).name.equalsIgnoreCase(name) && accounts.get(i).accountNo == fromAccount) {	
-				if (amt <= accounts.get(i).balance) {
+			if (accounts.get(i).getName().equalsIgnoreCase(name) && accounts.get(i).getAccountNo() == fromAccount) {
+				if (amt <= accounts.get(i).getBalance()) {
 					for (int j=0; j<accounts.size(); j++) {
-						if (accounts.get(j).name.equalsIgnoreCase(name) && accounts.get(j).accountNo == toAccount) {	
-							accounts.get(i).balance -= amt;
-							accounts.get(j).balance += amt;
-							string = "Account number " + accounts.get(i).accountNo + "'s new balance: " + accounts.get(i).balance + 
-										"\nAccount number " + accounts.get(j).accountNo + "'s new balance: " + accounts.get(j).balance;
+						if (accounts.get(j).getName().equalsIgnoreCase(name) && accounts.get(j).getAccountNo() == toAccount) {
+							accounts.get(i).setBalance(accounts.get(i).getBalance()-amt);
+							accounts.get(j).setBalance(accounts.get(j).getBalance()+amt);
+							string = "Account number " + accounts.get(i).getAccountNo() + "'s new balance: " + accounts.get(i).getBalance() +
+										"\nAccount number " + accounts.get(j).getAccountNo() + "'s new balance: " + accounts.get(j).getBalance();
 						} else {
 							string = "One of account number is wrong!";
 						}
 					}
 				} else {
-					string = "Insufficient funds! Balance: " + accounts.get(i).balance;
+					string = "Insufficient funds! Balance: " + accounts.get(i).getBalance();
 				}
 			}
 		}
--- Project 02/HelloClient.java
+++ Project 02/HelloClient.java
@@ -3,19 +3,19 @@ import java.util.ArrayList;
 
 public class HelloClient {
 	public static void main(String args[]) {
-		
-		ArrayList<BankAccount> accounts = new ArrayList<BankAccount>();
-		
+
 		try {
 			int port = 16790;         
 			String host = "localhost";
 			String registryURL = "rmi://" + host + ":" + port + "/hello";
-			AccountInterface h = (AccountInterface)Naming.lookup(registryURL);
+			AccountInterface h = (AccountInterface) Naming.lookup(registryURL);
 			System.out.println("Lookup completed " );
-			
-			
-			
-			
+
+			// BankAccount accountOne = new BankAccount();
+			// accountOne.createAccount("Bob", 1000f);
+
+			String str = h.createAccount("Rodrigo", 10024f);
+			System.out.println(str);
 			
 		} 
 		
